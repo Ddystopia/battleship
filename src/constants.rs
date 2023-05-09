328a61761d1ac93daac6e46dd73ffe92d0cef410
icit <38162697+ic-it@users.noreply.github.com>
2023-05-09 20:38:06 +0200

    ?

--- src/constants.rs
+++ src/constants.rs
@@ -14,4 +14,3 @@ pub const TOP_BORDER_MASK: u128 = LINE;
 pub const BOT_BORDER_MASK: u128 = move_board(TOP_BORDER_MASK, BOARD_SIZE - 1, Direction::Down);
 pub const LEF_BORDER_MASK: u128 = transpose(TOP_BORDER_MASK);
 pub const RGT_BORDER_MASK: u128 = LEF_BORDER_MASK >> (BOARD_SIZE - 1);
-
--- src/game.rs
+++ src/game.rs
@@ -1,9 +1,16 @@
 use crate::board_api::create_surround_mask;
 
+const SHIPS_COUNT: usize = 5;
+
+// • авианосец - 5 ячеек(клеток);
+// • крейсер - 4 ячейки;
+// • разрушитель - 3 ячейки;
+// • подводная лодка - 3 ячейки;
+// • катер - 2 ячейки.
 #[derive(Default, Copy, Clone, Debug)]
 pub struct Game {
-    pub board_alpha: u128,
-    pub board_beta: u128,
+    pub alpha_ships: [u128; SHIPS_COUNT],
+    pub beta_ships: [u128; SHIPS_COUNT],
     pub shoots_alpha: u128,
     pub shoots_beta: u128,
     pub step: usize,
@@ -36,60 +43,88 @@ impl Game {
     /// * `shoot` - the shoot to be made.
     pub fn step(&mut self, shoot: u128) -> bool {
         let player: Player = self.step.into();
+        let other_player = self.other_player(player);
         self.step += 1;
+
         match player {
             Player::Alpha => {
                 self.shoots_alpha |= shoot;
-                self.board_beta & !self.shoots_alpha == 0
+                self.get_board(other_player) & !self.shoots_alpha == 0
             }
             Player::Beta => {
                 self.shoots_beta |= shoot;
-                self.board_alpha & !self.shoots_beta == 0
+                self.get_board(other_player) & !self.shoots_beta == 0
             }
         }
     }
 
+    fn other_player(&self, player: Player) -> Player {
+        match player {
+            Player::Alpha => Player::Beta,
+            Player::Beta => Player::Alpha,
+        }
+    }
+
+    pub fn get_board(&self, player: Player) -> u128 {
+        let mut board: u128 = 0;
+        for i in 0..SHIPS_COUNT {
+            board |= match player {
+                Player::Alpha => self.alpha_ships[i],
+                Player::Beta => self.beta_ships[i],
+            };
+        }
+        board
+    }
+
     pub fn can_place_ship(&self, player: Player, ship: u128) -> bool {
         let mask = create_surround_mask(ship);
-
-        let board = match player {
-            Player::Alpha => self.board_alpha,
-            Player::Beta => self.board_beta,
-        };
+        let board = self.get_board(player);
 
         mask & board == 0
     }
 
-    pub fn add_ship(&mut self, player: Player, ship: u128) -> Result<(), ()> {
+    pub fn add_ship(&mut self, player: Player, ship: u128, layer: u8) -> Result<(), ()> {
         if !self.can_place_ship(player, ship) {
             return Err(());
         }
 
         match player {
-            Player::Alpha => self.board_alpha |= ship,
-            Player::Beta => self.board_beta |= ship,
+            Player::Alpha => {
+                self.alpha_ships[layer as usize] |= ship;
+            }
+            Player::Beta => {
+                self.beta_ships[layer as usize] |= ship;
+            }
         };
         Ok(())
     }
 
-    pub fn add_ship_unchecked(&mut self, player: Player, ship: u128) {
+    pub fn add_ship_unchecked(&mut self, player: Player, ship: u128, layer: u8) {
         match player {
-            Player::Alpha => self.board_alpha |= ship,
-            Player::Beta => self.board_beta |= ship,
+            Player::Alpha => {
+                self.alpha_ships[layer as usize] |= ship;
+            }
+            Player::Beta => {
+                self.beta_ships[layer as usize] |= ship;
+            }
         }
     }
 
     pub fn get_hitted(&self, player: Player) -> u128 {
+        let other_player = self.other_player(player);
+
         match player {
-            Player::Alpha => self.shoots_alpha & self.board_beta,
-            Player::Beta => self.shoots_beta & self.board_alpha,
+            Player::Alpha => self.shoots_alpha & self.get_board(other_player),
+            Player::Beta => self.shoots_beta & self.get_board(other_player),
         }
     }
 
     pub fn get_intact(&self, player: Player) -> u128 {
+        let other_player = self.other_player(player);
+
         match player {
-            Player::Alpha => self.board_beta & !self.shoots_alpha,
-            Player::Beta => self.board_alpha & !self.shoots_beta,
+            Player::Alpha => self.get_board(other_player) & !self.shoots_alpha,
+            Player::Beta => self.get_board(other_player) & !self.shoots_beta,
         }
     }
 
@@ -114,10 +149,10 @@ mod test {
         let ship = transpose(create_ship(4));
         let ship = move_board(ship, 1, Direction::Down);
         assert!(game.can_place_ship(Player::Alpha, ship));
-        game.add_ship(Player::Alpha, ship).unwrap();
+        game.add_ship(Player::Alpha, ship, 0).unwrap();
         let ship = create_ship(3);
         assert!(!game.can_place_ship(Player::Alpha, ship));
-        assert_eq!(game.add_ship(Player::Alpha, ship), Err(()));
+        assert_eq!(game.add_ship(Player::Alpha, ship, 0), Err(()));
     }
 
     #[test]
@@ -126,11 +161,11 @@ mod test {
         let ship = transpose(create_ship(4));
         let ship = move_board(ship, 3, Direction::Down);
         assert!(game.can_place_ship(Player::Alpha, ship));
-        game.add_ship(Player::Alpha, ship).unwrap();
+        game.add_ship(Player::Alpha, ship, 1).unwrap();
         let ship = create_ship(3);
         let ship = move_board(ship, 1, Direction::Right);
         assert!(game.can_place_ship(Player::Alpha, ship));
-        assert_eq!(game.add_ship(Player::Alpha, ship), Ok(()));
+        assert_eq!(game.add_ship(Player::Alpha, ship, 1), Ok(()));
     }
 
     #[test]
@@ -139,10 +174,10 @@ mod test {
         let ship = transpose(create_ship(4));
         let ship = move_board(ship, 2, Direction::Down);
         assert!(game.can_place_ship(Player::Alpha, ship));
-        game.add_ship(Player::Alpha, ship).unwrap();
+        game.add_ship(Player::Alpha, ship, 2).unwrap();
         let ship = create_ship(3);
         let ship = move_board(ship, 1, Direction::Right);
         assert!(game.can_place_ship(Player::Alpha, ship));
-        assert_eq!(game.add_ship(Player::Alpha, ship), Ok(()));
+        assert_eq!(game.add_ship(Player::Alpha, ship, 2), Ok(()));
     }
 }
